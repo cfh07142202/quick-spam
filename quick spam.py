da903df8de7f4f1041f09ede6ed7ac0c926366d7
cfh07142202 <cfh07142202@gmail.com>
2026-08-08 10:28:26 +0100

    quick spam.py
    fixed: the program crashing if a number is null

--- quick spam.py
+++ quick spam.py
@@ -14,14 +14,14 @@ def tool_opps():
 def fast_mode():
     for _ in range(How_many_times_do_you_want_to_spam):
         py.write(what_word_to_SPAM)
-        ti.sleep(intkey)
+        ti.sleep(intervalkey)
         #very tiny hard coded break just so it picks it up
         ti.sleep(0.1)
         py.press("enter")
 def slow_mode():
     for _ in range(How_many_times_do_you_want_to_spam):
         py.write(what_word_to_SPAM, interval=0.1)
-        ti.sleep(intkey)
+        ti.sleep(intervalkey)
         #very tiny hard coded break just so it picks it up
         ti.sleep(0.1)
         py.press("enter")
@@ -30,20 +30,37 @@ def clear():
     sys.stdout.flush()
 tool_opps()
 what_tool = input(":")
+while what_tool == "":
+    print(Fore.RED + "this can't be null")
+    what_tool = input(":")
 if what_tool == "4":
     print("mouse position")
     print(py.position())
     print("screen size")
     print(py.size())
 elif what_tool == "2":
+    clear()
     what_word_to_SPAM = input("what word would you like to spam:")
     while what_word_to_SPAM == "":
         clear()
         print(Fore.RED + "this can't be null")
         what_word_to_SPAM = input("what word would you like to spam:")
-    How_many_times_do_you_want_to_spam = int(input("how many times would you like to spam:"))
     clear()
-    intkey = float(input("interval between key press:"))
+    while True:
+        try:
+            How_many_times_do_you_want_to_spam = int(input("how many times would you like to spam:"))
+            break
+        except ValueError:
+            clear()
+            print(Fore.RED + " invaild number")
+    clear()
+    while True:
+        try:
+            intervalkey = float(input("interval between key press:"))
+            break
+        except ValueError:
+            clear()
+            print(Fore.RED + "invaild number")
     clear()
     f_or_slow = input("fast or slow mode? f/s:")
     # just trying a dif way to do this i know this isnt the cleanest way of doing it
@@ -51,6 +68,8 @@ elif what_tool == "2":
         fast_or_slow = True
     elif f_or_slow == "s":
         fast_or_slow = False
+    if f_or_slow == "":
+        print("auto set to slow mode")
     if fast_or_slow:
         print("press s to start")
         #just to be safe it dosen't get in the way in your chat box
@@ -63,6 +82,9 @@ elif what_tool == "2":
         key.wait("s")
         py.press("backspace")
         slow_mode()
+elif what_tool == "3":
+    clear()
+    what_button_to_spam = input("what button do you want to spam?")
 else:
-    print(Fore.RED + "this is not vaild")
+    print(Fore.RED + f"{what_tool} is not vaild")
 input("\n")
